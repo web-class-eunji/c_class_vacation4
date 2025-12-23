0eb466a20337ad040062cf20c7c273d685cd06e0
moeunji <moeunji12@gmail.com>
2025-12-23 16:06:24 +0900

    day1

---
+++ c_class_vacation4/기본구조.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World! 안녕하세요 저는 홍길동\n");
+	printf("오늘은 월요일입니다.");
+	printf("은지\n봄이\n\n종이\n대박이\n두두\n"); 
+	printf("강아지\t고양이\t토끼\n");
+	printf("dog\tcat\trabbit\n");
+	printf("12345678\t1234\n");
+	printf("강아지는 \"멍멍\" 고양이는 \"야옹\" 토끼는 몰라");
+	return 0;
+
+}
---
+++ c_class_vacation4/영어다이아몬드.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("\t\tA\n");
+	printf("\tB\t\tC\n");
+	printf("D\t\tE\t\tF\n");
+	printf("\tG\t\tH\n");
+	printf("\t\tI\n");
+	return 0;
+
+}
